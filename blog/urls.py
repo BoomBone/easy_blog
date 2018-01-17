382ebd339ef2166d712fdc5f15bbfd429eec632c
Ting <ticccccc@163.com>
2018-01-17 16:35:04 +0800

    博客文章页面开发

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index/', views.index),
+    path('article/<int:article_id>', views.article_page,),
 ]

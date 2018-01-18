f448a11b0507502b2caa1dfd33ddabb85eb2bacf
Ting <ticccccc@163.com>
2018-01-18 10:57:15 +0800

    博客撰写页面1

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from . import models
 
 
@@ -11,3 +12,14 @@ def index(request):
 def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
+
+
+def edit_page(request):
+    return render(request, 'blog/edit_page.html')
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'Title')
+    content = request.POST.get('content', 'CONTENT')
+    models.Article.objects.create(title=title, content=content)
+    return HttpResponseRedirect('/blog/index/')
